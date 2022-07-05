76225e17a655ccb7527862ead4dce4d7ee5a858a
Terence625 <terencetianterence@gmail.com>
2022-07-05 22:25:28 +1000

    [Terence] finish challenge 2

---
+++ challenge2.js
@@ -0,0 +1,22 @@
+function highestOccurChar(s) {
+  let charList = s.toLowerCase().split("");
+  let charOccurMap = new Map();
+  charList.forEach((item) => {
+    if (charOccurMap.has(item)) {
+      charOccurMap.set(item, charOccurMap.get(item) + 1);
+    } else {
+      charOccurMap.set(item, 1);
+    }
+  });
+  let highestOccurChar;
+  let highestOccurNum = 0;
+  charOccurMap.forEach((value, key) => {
+    if (value > highestOccurNum) {
+      highestOccurChar = key;
+      highestOccurNum = value;
+    }
+  });
+  return highestOccurChar;
+}
+
+console.log(highestOccurChar("Character"));
