216b555bf8241e7ba559fdd6e651b3b2474b8184
Terence625 <terencetianterence@gmail.com>
2022-07-08 11:51:31 +1000

    [Terence] finish challenge1

--- challenge1.js
+++ challenge1.js
@@ -11,11 +11,30 @@ function getFibonacci(n) {
 
 /**
  * Check if a number is a fibonacci number and get the closet index n in fibonacci sequence
- * @param n: input number
+ * @param x: input number
  * @return: string indicate if the number is a fibonacci number and the closet index n in fibonacci sequence
  */
 function isFibonacci(x) {
-  let fibonacciNum = 0;
+  let fibonacciArr = [0, 1];
+  while (x > fibonacciArr.at(-1)) {
+    fibonacciArr.push(fibonacciArr.at(-1) + fibonacciArr.at(-2));
+  }
+  let fibonacciArrLastNum = fibonacciArr.at(-1);
+  let fibonacciArrASecLastNum = fibonacciArr.at(-2);
+  if (x === 0) return `${x} is the 1st fibonacci number`;
+  else if (x === 1) return `${x} is the 2nd or 3rd fibonacci number`;
+  else if (fibonacciArrLastNum === x)
+    return `${x} is the ${fibonacciArr.length}th fibonacci number`;
+  else if (x - fibonacciArrASecLastNum === fibonacciArrLastNum - x)
+    return `${x} is not a fibonacci number, the closest index in the fibonacci sequence is ${
+      fibonacciArr.length - 1
+    } or ${fibonacciArr.length}`;
+  else if (x - fibonacciArrASecLastNum > fibonacciArrLastNum - x)
+    return `${x} is not a fibonacci number, the closest index in the fibonacci sequence is ${fibonacciArr.length}`;
+  else
+    return `${x} is not a fibonacci number, the closest index in the fibonacci sequence is ${
+      fibonacciArr.length - 1
+    }`;
 }
-
 console.log(getFibonacci(8));
+console.log(isFibonacci(2585));
